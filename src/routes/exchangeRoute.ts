982d3424377043c30029316771008db8abac4d65
Mikel Sanchez <misanche@redhat.com>
2018-09-14 13:33:59 +0200

    Updated v3

---
+++ src/routes/exchangeRoute.ts
@@ -0,0 +1,5 @@
+/* import { Router } from "express";
+import { getExchange } from "../controllers/exchange";
+import { asyncHandler } from "../lib/asyncHandler";
+export const exchangeGet  = Router().use("/", asyncHandler(getExchange, "getExchange"));
+ */
