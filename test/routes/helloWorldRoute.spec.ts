4452c48c90811512fd6071bf5083a68cadb0fa91
Mikel Sanchez <misanche@redhat.com>
2018-09-17 18:27:40 +0200

    fix

--- test/routes/helloWorldRoute.spec.ts
+++ test/routes/helloWorldRoute.spec.ts
@@ -1,9 +1,7 @@
 import chaiHttp = require("chai-http");
 import { initSwaggerMiddlware } from "../../src/middlewares/swagger";
 import app from "../../src/application";
-// import { Exchange  } from "api/dist/node-models/hello_world/models";
 import * as chai from "chai";
-// import * as nock from "nock";
 const expect = chai.expect;
 chai.use(chaiHttp);
 
