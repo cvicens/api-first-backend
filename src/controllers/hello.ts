0268d4ea241f5afd6e1bedaf5b841907288192d1
Mikel Sanchez <misanche@redhat.com>
2018-09-19 21:10:32 +0200

    Updated code

--- src/controllers/hello.ts
+++ src/controllers/hello.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express";
 import * as P from "bluebird";
 import { FesDebug } from "../log";
-import { Hello } from "api/dist/node-models/hello_world/hello";
+import { Hello, Greeting } from "api/dist/node-models/hello_world/models";
 
 const debug = new FesDebug("src:controllers:getHelloWorld");
 
 export async function getHelloWorld(req: Request, res: Response): P<any> {
-    const greeting =
-    req.swagger.params && req.swagger.params.greeting.value ? req.swagger.params.greeting.value : "World";
-    debug.log("Greeting: ", greeting);
-    res.send({ msg: "hello " + greeting} as Hello);
+    const name = req.swagger.params.name.value;
+    const surname = req.swagger.params.surname.value;
+    res.send({ msg: "hello " + name + " " + surname} as Hello);
+}
+
+export async function postHelloWorld(req: Request, res: Response): P<any> {
+    const greeting: Greeting = req.swagger.params.greeting.value;
+    const surname = greeting.surname ? " " + greeting.surname : "";
+    res.send({ msg: "hello " + greeting.name + surname} as Hello);
 }
