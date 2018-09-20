f59a1326e040b11cba7e733afac2398923604ee0
Mikel Sanchez <misanche@redhat.com>
2018-09-20 09:32:31 +0200

    change code

--- src/routes/helloWorldRoute.ts
+++ src/routes/helloWorldRoute.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import {getHelloWorld, postHelloWorld} from "../controllers/hello";
+import { getHelloWorld, postHelloWorld} from "../controllers/hello";
 import { asyncHandler } from "../lib/asyncHandler";
-export const helloWorldGet  = Router().use("/", asyncHandler(getHelloWorld, "helloWorldGet"));
-export const helloWorldPost  = Router().use("/", asyncHandler(postHelloWorld, "helloWorldPost"));
+
+export const   helloWorldGet = Router().use("/", asyncHandler(getHelloWorld, "exchangeGet"));
+export const   helloWorldPost = Router().use("/", asyncHandler(postHelloWorld, "exchangeGet"));
